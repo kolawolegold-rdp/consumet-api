20bbf15d67106de71129914b9a08738014fc226f
kolawolegold-rdp <kolazephyr@gmail.com>
2026-05-29 12:14:50 +0100

    ...

--- src/routes/movies/flixhq.ts
+++ src/routes/movies/flixhq.ts
@@ -1,6 +1,6 @@
 import { FastifyRequest, FastifyReply, FastifyInstance, RegisterOptions } from 'fastify';
 import { MOVIES } from '@consumet/extensions';
-import { StreamingServers } from '@consumet/extensions/dist/models';
+import { StreamingServers } from '@consumet/extensions/dist/models/index.js';
 
 const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
   const flixhq = new MOVIES.FlixHQ();
